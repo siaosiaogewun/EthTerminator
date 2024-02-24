b71630adad776326dcec1487870f6b587ed14434
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-25 06:20:58 +0800

    3.1.8

--- 0002ehter5版本生成和小狐狸相同的地址.js
+++ 0002ehter5版本生成和小狐狸相同的地址.js
@@ -1,12 +1,12 @@
 const { ethers }  = require('ethers');
 
 const mnemonic = "diary steak forest resist pumpkin grass outer punch bone saddle method umbrella";
-const hdNode = ethers.HDNodeWallet.fromPhrase(mnemonic);
+let node = ethers.utils.HDNode.fromMnemonic(mnemonic)
 
-console.log(hdNode);
+console.log(node);
 
-const secondAccount = hdNode.derivePath(`44'/60'/0'/0/0`);
-const thirdAccount = hdNode.derivePath(`44'/60'/0'/0/1`);
+const secondAccount = node.derivePath(`m/44'/60'/0'/0/0`);
+const thirdAccount = node.derivePath(`m/44'/60'/0'/0/1`);
 
 console.log(secondAccount.address);
 console.log(thirdAccount.address);
